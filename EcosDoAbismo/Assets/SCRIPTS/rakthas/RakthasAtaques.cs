bae9ce1ad7188ed3fbbc3af21a4e1c601fbcdffa
pietroguilerme <tccpgifrn@gmail.com>
2026-09-24 20:05:37 -0300

    ataque1 feito do rakthas //pietro

--- EcosDoAbismo/Assets/SCRIPTS/rakthas/RakthasAtaques.cs
+++ EcosDoAbismo/Assets/SCRIPTS/rakthas/RakthasAtaques.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class RakthasAtaques : MonoBehaviour
 {
     public GameObject hitboxRaio;
+    
 
     private bool atacando = false;
     private SpriteRenderer spriteRenderer;
@@ -11,6 +12,10 @@ public class RakthasAtaques : MonoBehaviour
     public float posicaoXDireita = 0.352f;
     public float posicaoXEsquerda = -1.054f;
 
+    [Header("Hitboxes da Onda")]
+    public BoxCollider2D hitboxOndaDireita;
+    public BoxCollider2D hitboxOndaEsquerda;
+
     public bool EstaAtacando => atacando;
 
     private void Awake()
@@ -18,22 +23,35 @@ public class RakthasAtaques : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    private void Update()
+    
+private void Update()
+{
+    AtualizarPosicaoHitboxRaio();
+
+    // TESTE DO RAIO
+    if (Input.GetKeyDown(KeyCode.N) && !atacando)
     {
-        AtualizarPosicaoHitboxRaio();
+        atacando = true;
 
-        // TEMPORÁRIO PARA TESTAR O RAIO
-        if (Input.GetKeyDown(KeyCode.M) && !atacando)
-        {
-            atacando = true;
+        Animator animator = GetComponent<Animator>();
 
-            Animator animator = GetComponent<Animator>();
-
-            animator.speed = 1f;
-            animator.SetBool("estaCorrendo", false);
-            animator.SetTrigger("Raio");
-        }
+        animator.speed = 1f;
+        animator.SetBool("estaCorrendo", false);
+        animator.SetTrigger("Raio");
     }
+
+    // TESTE DA ONDA DE FOGO
+    if (Input.GetKeyDown(KeyCode.M) && !atacando)
+    {
+        atacando = true;
+
+        Animator animator = GetComponent<Animator>();
+
+        animator.speed = 1f;
+        animator.SetBool("estaCorrendo", false);
+        animator.SetTrigger("Ataque1");
+    }
+}
 
     private void AtualizarPosicaoHitboxRaio()
     {
@@ -73,4 +91,44 @@ public class RakthasAtaques : MonoBehaviour
     {
         atacando = false;
     }
+
+    
+
+public void AtivarHitboxOnda()
+{
+    if (hitboxOndaDireita == null || hitboxOndaEsquerda == null)
+        return;
+
+    hitboxOndaDireita.enabled = false;
+    hitboxOndaEsquerda.enabled = false;
+
+    BoxCollider2D hitboxAtual = spriteRenderer.flipX
+        ? hitboxOndaEsquerda
+        : hitboxOndaDireita;
+
+    RakthasOndaHitbox onda =
+        hitboxAtual.GetComponent<RakthasOndaHitbox>();
+
+    if (onda != null)
+        onda.ReiniciarAcertos();
+
+    hitboxAtual.enabled = true;
+
+    if (onda != null)
+        onda.VerificarDano();
+}
+
+public void DesativarHitboxOnda()
+{
+    if (hitboxOndaDireita != null)
+        hitboxOndaDireita.enabled = false;
+
+    if (hitboxOndaEsquerda != null)
+        hitboxOndaEsquerda.enabled = false;
+}
+
+public void FinalizarAtaque1()
+{
+    atacando = false;
+}
 }
